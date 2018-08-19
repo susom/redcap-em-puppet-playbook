cb1b18a524163e07b8840fb679f8560823de4281
Andrew Martin <andy123@stanford.edu>
2018-08-19 09:41:01 -0700

    renamed log to emLog

--- Playbook.php
+++ Playbook.php
@@ -280,20 +280,20 @@ class Playbook extends \ExternalModules\AbstractExternalModule
      */
     function emLog() {
         $emLogger = \ExternalModules\ExternalModules::getModuleInstance('em_logger');
-        $emLogger->log($this->PREFIX, func_get_args(), "INFO");
+        $emLogger->emLog($this->PREFIX, func_get_args(), "INFO");
     }
 
     function emDebug() {
         // Check if debug enabled
         if ($this->getSystemSetting('enable-system-debug-logging') || $this->getProjectSetting('enable-project-debug-logging')) {
             $emLogger = \ExternalModules\ExternalModules::getModuleInstance('em_logger');
-            $emLogger->log($this->PREFIX, func_get_args(), "DEBUG");
+            $emLogger->emLog($this->PREFIX, func_get_args(), "DEBUG");
         }
     }
 
     function emError() {
         $emLogger = \ExternalModules\ExternalModules::getModuleInstance('em_logger');
-        $emLogger->log($this->PREFIX, func_get_args(), "ERROR");
+        $emLogger->emLog($this->PREFIX, func_get_args(), "ERROR");
     }
 
 }
