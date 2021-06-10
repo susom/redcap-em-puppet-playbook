dbde2a7bc15e8b72264c09b7ad6a42e105e08b57
Ihab Zeedia <ihab.zeedia@gmail.cm>
2021-06-10 13:06:34 -0700

    remove proxy server.

--- Playbook.php
+++ Playbook.php
@@ -129,11 +129,7 @@ class Playbook extends \ExternalModules\AbstractExternalModule
 
 
 	        $guzzle_response = $client->post($url, [
-                \GuzzleHttp\RequestOptions::JSON => $body,
-                'proxy' => [
-                    'http' => 'http://tds-zero-p03.stanford.edu', // Use this proxy with "http"
-                    'https' => 'https://tds-zero-p03.stanford.edu', // Use this proxy with "https",
-                ]
+                \GuzzleHttp\RequestOptions::JSON => $body
             ]);
 	        // $response = http_post($url, $body, $timeout, $context_type);
 	        $response = $guzzle_response->getBody();
